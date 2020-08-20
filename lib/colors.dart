367ada37abd010ad85c8be82256862d48c979da8
Kalpana <kalpana@foodmonk.com>
2020-08-19 22:58:12 -0700

    did some changes

--- lib/colors.dart
+++ lib/colors.dart
@@ -2,15 +2,11 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 
-const primaryLight = Colors.greenAccent;
-const primaryColor = const Color(0xFF08C5A7);
+const primaryLight = Colors.blueAccent;
+const primaryColor =  Colors.blue;
 const secondaryColor = Colors.grey;
-const backgroundColor = const Color(0xFFE5E5E5);
-//const textBlackColor = const Color(0xFF31456A);
-//const textGreyColor = const Color(0xFF8E9AAF);
-//const blueColor = const Color(0xFF3BBDED);
+const backgroundColor = const Color(0xFF7A9BEE);
 
-const themeColor = const Color(0xFF08C5A7);
 const greyColor = Colors.grey;
 const blackColor = Colors.black;
 const whiteColor = Colors.white;
